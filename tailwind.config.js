7f24afff46fd81875e03ca742052f95c3b01d456
hyra <stef@mindthecode.com>
2025-01-18 16:17:14 +0100

    Update

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./layout/*.liquid', './templates/*.liquid', './sections/*.liquid', './snippets/*.liquid'],
+  theme: {
+    extend: {
+      fontFamily: {
+        bannertype: ['BannerType2', 'sans-serif'],
+        rubik: ['Rubik', 'sans-serif'],
+        p22mackinac: ['p22_mackinac_probold', 'sans-serif'],
+      },
+      backgroundImage: {
+        dotted: 'radial-gradient(circle, #471313 2px, transparent 2px)',
+      },
+      colors: {
+        'nabati-pink': '#FD2B53',
+        'peanut-brown': '#470101',
+        'bamboo-beige': '#F1E3BC',
+        'coconut-white': '#FFFFFF',
+      },
+    },
+  },
+  plugins: [],
+};
